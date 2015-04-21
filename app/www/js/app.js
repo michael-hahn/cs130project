50b43f485c02ab44f8a2f7edd89dc116bf5814d2
Jason Tieu <tieujason330@gmail.com>
2015-04-20 23:41:40 -0700

    initial implementation of profile

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -110,6 +110,16 @@ angular.module('starter', ['firebase', 'ngCordova', 'ionic', 'starter.controller
     params: {'imageData' : null }
   })
 
+  .state("app.profile", {
+    url: "/profile",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/profile.html",
+        controller: 'ProfileController'
+      }
+    }
+  })
+
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/login');
 });
