a15925e6e502d3aa8e1828dfdb129f652071a8c3
Michael Hahn <mike.han@hotmail.com>
2015-05-01 12:30:42 -0700

    Add Join Event controller.
    1. The user is now able to join an existing even given that he/she provides correct event name, host user's email address and password.
    2. If the user provides an invalid event name, he/she will be informed
    3. If the user provides an invalid event name, host user's email combo, he/she will be informed
    4. If the user provides a valid event name and host user's email combo but the password is wrong, he/she will be informed
    5. If the user attempts to join an event that he/she is already joined, he/she will be informed
    6. Once the event is joined, the event ID will be added to the user's data in our data base with the tag "guest"
    7. Once the event is joined, the user will be directed to the image panel
    8. So far, the user will not be informed if he/she provides a valid event name but invalid host email address. This is because I am still unable to search through our user data base and verify the user email address. However, NO bugs will be introduced to this lack of information since if the host email address is wrong, the combo will be wrong as well and user will be informed as in (3.)
    9. Code hasn't been cleaned up and documented. Will do this later.

--- app/www/js/controllers/joinEventController.js
+++ app/www/js/controllers/joinEventController.js
@@ -9,5 +9,187 @@
 **/
 angular.module('starter')
 
-.controller('joinEventController', function($scope, $stateParams, firebaseObject) {
+.controller('joinEventController', function($scope, $firebaseArray, $q, $state) {
+  var fbAuth = firebaseObject.getAuth();
+
+
+  if(fbAuth) {
+      var myEventsReference = firebaseObject.child("users/" + fbAuth.uid + "/Events");
+      var eventReference = firebaseObject.child('Events');
+    }
+    else {
+      $state.go("login");
+    }
+
+  function eventExist(eventName) {
+    return $q(function(resolve, reject){
+      var eventReference = firebaseObject.child('Events');
+      eventReference.orderByChild("Name").equalTo(eventName).on("value", function(oneEvent){
+          if (oneEvent.val() != null){
+            resolve();
+            //alert("At least one Event with the same name exists");
+          }
+          else {
+            alert("No Events Matched. Try Again");
+            reject();
+          } 
+        });       
+    });
+  }
+
+  function userHasEvents() {
+    return $q(function(resolve, reject) {
+      var userInfo = firebaseObject.child("users/" + fbAuth.uid);
+      userInfo.once('value', function(dataSnapshot) {
+        if (dataSnapshot.hasChild("Events")) {
+          resolve();
+        //  alert("You have some event(s)");
+        }
+        else {
+          reject();
+        //  alert("You don't have any events");
+        }
+      });
+    });
+  }
+
+  function eventNotJoined(eventID) {
+    return $q(function(resolve, reject){
+    //   var userEvents = firebaseObject.child("users/" + fbAuth.uid + "/Events");
+    //   userEvents.equalTo(eventName).on("value", function(oneEvent){
+    //     var eventinfo = oneEvent.val();
+    //     if(oneEvent.val() != null) {
+    //       reject();
+    //       alert("You have joined this event already!");
+    //     }
+    //     else {
+    //       resolve();
+    //       alert("You can join this event!");
+    //     }
+    //   });
+    // });
+        var userInfo = firebaseObject.child("users/" + fbAuth.uid);
+        if (!userHasEvents()) {
+          resolve();
+         // alert("You can join this event!");
+        }
+        else {
+          //var eventID = getEventID(eventName, userEmail);
+          userInfo.once('value', function(dataSnapshot) {
+            if (dataSnapshot.child("Events/" + eventID).exists()) {
+              reject();
+             // alert("You have joined this event already!");
+            }
+            else {
+              resolve();
+            //alert("Another event to be joined!");
+            }
+          });
+        }
+    });
+  }
+
+  // Get the name of the event with a given ID
+  function getEmailOfUserWithID(userID) {
+    return $q(function(resolve, reject) {
+      firebaseObject.child("users/" + userID).once("value", function(userData) {
+        resolve( userData.val()["email"] );
+      });
+    });
+  }
+
+  function validEventHost(userEmail) {
+    return $q(function(resolve, reject){
+      var userReference = firebaseObject.child('users');
+      userReference.orderByChild("email").equalTo(userEmail).on("value", function(oneHost){
+          if (oneHost.val() == getEmailOfUserWithID(fbAuth.uid)) {
+            reject();
+            alert("You cannot join your own event!");
+          }
+          else if (oneHost.val() != null) {
+            resolve();
+            alert("Host Email is Valid");
+          }
+          else {
+            alert("NO USER MATCHED");
+            reject();
+          } 
+        }); 
+    });
+  }
+
+  var targetEvent = null;
+  var targetEventID = null;
+
+  // function getEventID(eventName, userEmail) {
+  //   var eventReference = firebaseObject.child('Events');
+  //   return $q(function(resolve, reject) {
+  //     eventReference.once('value', function(allEvents){
+  //       allEvents.forEach(function(singleEvent){
+  //         var singleEventName = singleEvent.child('Name').val();
+  //         var singleEventHost = singleEvent.child('Host').val();
+  //         if(singleEventName == eventName && singleEventHost == userEmail){
+  //           targetEvent = singleEvent.val();
+  //           resolve(singleEvent.key());
+  //           return true;
+  //         }
+  //       });
+  //         if (targetEvent == null) {
+  //         alert("NO EVENT FOUND");
+  //         reject();
+  //         }
+  //     });
+  //   });
+  // }
+
+  function getEventPassword(eventName, userEmail) {
+    targetEvent = null;
+    var eventReference = firebaseObject.child('Events');
+    return $q(function(resolve, reject){
+      eventReference.once('value', function(allEvents){
+        allEvents.forEach(function(singleEvent){
+          var singleEventName = singleEvent.child('Name').val();
+          var singleEventHost = singleEvent.child('Host').val();
+          if(singleEventName == eventName && singleEventHost == userEmail){
+            targetEvent = singleEvent.val();
+            targetEventID = singleEvent.key();
+            resolve(targetEvent.Password);
+            return true;
+          }
+        });
+        if (targetEvent == null) {
+          alert("No events associated with this user or invalid user email");
+          reject();
+        }
+      });
+    });
+  }
+
+
+  $scope.joinEvent = function(eventName, userEmail, password) {
+      eventExist(eventName).then(function() {
+        //validEventHost(userEmail).then(function(){
+          // eventNotJoined(eventName, userEmail).then(function(){
+          //   getEventPassword(eventName, userEmail).then(function(eventPassword) {
+            getEventPassword(eventName, userEmail).then(function(eventPassword) {
+                eventNotJoined(targetEventID).then(function(){
+                if (password == eventPassword) {
+
+                  myEventsReference.child(targetEventID).set("guest");
+                  alert("Event joined!");
+                  $state.go("app.images");
+              }
+              else {
+                alert("Wrong Passowrd");
+              }
+            }, function() {
+              alert("Cannot join the event");
+            });
+          }); 
+      });
+  }
+
+
+
+
 })
