55fcd90d67479e4d77e76d79bf70e1c59435c507
drosales57 <Daniel@wifi-131-179-33-180.host.ucla.edu>
2015-05-07 18:57:50 -0700

    Revert "Add the ability to like pictures"
    This reverts commit d40cdf017b2caca0ee582acbe87c0f9870fc7f7a.
    
    will commit once I fix the bug.

--- app/www/js/controllers/eventPageController.js
+++ app/www/js/controllers/eventPageController.js
@@ -15,7 +15,6 @@ angular.module('starter')
   $scope.eventHostEmail = $stateParams.eventHostEmail;
   $scope.eventActive = $stateParams.eventActive;
   $scope.userEmail = $stateParams.userEmail;
-  $scope.numLikes = 0;
 
   $timeout(function() {}, 0);
 
@@ -88,15 +87,11 @@ angular.module('starter')
         var minutes = "0" + date.getMinutes();
         var seconds = "0" + date.getSeconds();
         var formattedTime = hours + ':' + minutes.substr(minutes.length - 2) + ':' + seconds.substr(seconds.length - 2);
-        var likedbyList = {};
-        likedBy[fbAuth.uid] = 0;
-
+        
         syncArray.$add({
           image: "data:image/jpeg;base64," + imageData,
           user: fbAuth.uid,
-          time: formattedTime,
-          numLikes: 0, // 0 likes for a new photo
-          likedBy: likedbyList // should be empty object ({}) but FB doesn't allow it.
+          time: formattedTime
         }).then(function() {
           alert("Image has been uploaded!");
         });
@@ -149,49 +144,5 @@ angular.module('starter')
         hostEmail : $scope.eventHostEmail,
         eventID : $scope.eventID })
   }
-
-  $scope.like = function(images, im) {
-
-    if (im.likedBy !== undefined) {
-
-      if (fbAuth.uid === im.user) {
-        if (im.likedBy[fbAuth.uid] === 0) {
-          im.numLikes++;
-          im.likedBy[fbAuth.uid] = 1;
-          images.$save(im);
-        }
-      } else if (im.likedBy[fbAuth.uid] === undefined) {
-        
-        if (im.numLikes !== undefined)
-          im.numLikes++;
-
-        im.likedBy[fbAuth.uid] = 1;
-        images.$save(im)
-      }
-    }
-  }
-
-  $scope.isLiked = function(im) {
-    if (im.likedBy !== undefined) {
-
-      if (im.likedBy[fbAuth.uid] === 1 ) {
-        return true;
-      } 
-    }
-    return false;
-  }
-
-  $scope.getLikeIcon = function(im) {
-    if (im.likedBy !== undefined) {
-
-      if (im.likedBy[fbAuth.uid] === 1 ) {
-        return "button button-icon icon ion-android-checkbox";
-      } 
-      else
-        return "button button-icon icon ion-android-done";
-    }
-    return false;
-  }
-
 
 })
